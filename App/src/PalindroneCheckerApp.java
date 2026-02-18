6da3f159411daa208c378374167936ac225f4bf7
samiulislam8723-cloud <samiulislam8723@gmail.com>
2026-02-18 16:16:35 +0530

    Succes

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,6 +1,7 @@
 public class PalindroneCheckerApp
 {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Welcome to Palindrone Checker App Management System");
+        System.out.println("Modify this logic to usecase 2");
     }
 }
